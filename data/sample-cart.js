0288771466ada30a1414513dfa881d3680bdc4c4
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-06 14:46:14 -0700

    sample object literals made

---
+++ data/sample-cart.js
@@ -0,0 +1,31 @@
+//Sample object literals
+
+const bat = {
+    id: 'bat',
+    name: 'Baseball Bat',
+    image: '../assets/bat.png',
+    category: 'Baseball',
+    description: '20oz Wooden Bat',
+    price: 50.00,
+};
+
+const mitt = {
+    id: 'mitt',
+    name: 'Baseball Mitt',
+    image: '../assets/mitt.png',
+    category: 'Baseball',
+    description: 'Leather Outfielders Mitt',
+    price: 40.00,
+};
+
+const catchersMask = {
+    id: 'catchersMask',
+    name: 'Catcher\'s Mask',
+    image: '../assets/mask.png',
+    category: 'Baseball',
+    description: 'Wire frame catchers mask',
+    price: 30.00,
+};
+
+//Sample Array
+export const sampleArray = [bat, mitt, catchersMask];
